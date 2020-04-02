3e462666d88f1fcaf1f39bd45014172791e17dd7
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-04-01 23:53:52 -0400

    fix test

--- src/main/java/com/hw/helper/SecurityProfile.java
+++ src/main/java/com/hw/helper/SecurityProfile.java
@@ -6,13 +6,20 @@ import lombok.Data;
 public class SecurityProfile {
     private String expression;
 
-    private String resourceID;
+    private String resourceId;
 
-    private String path;
+    private String lookupPath;
 
     private String method;
 
     private String url;
 
     private Long id;
+    private String scheme;
+    private String userInfo;
+    private String host;
+    private Integer port;
+    private String path;
+    private String query;
+    private String fragment;
 }
