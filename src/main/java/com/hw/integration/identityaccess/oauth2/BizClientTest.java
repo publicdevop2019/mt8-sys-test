3a9fafb4eefac79a00b73a2cd97b7d195e4a786d
publicdevop2019 <publicdevop2019hw@gmail.com>
2021-01-15 17:43:36 -0500

    add new client test

--- src/main/java/com/hw/integration/identityaccess/oauth2/BizClientTest.java
+++ src/main/java/com/hw/integration/identityaccess/oauth2/BizClientTest.java
@@ -17,12 +17,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.*;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.UUID;
+import java.util.*;
 
 import static com.hw.helper.UserAction.*;
+import static com.hw.integration.identityaccess.oauth2.BIzUserTest.RESOURCE_OWNER;
 
 @RunWith(SpringRunner.class)
 @Slf4j
@@ -263,6 +264,135 @@ public class BizClientTest {
         ResponseEntity<DefaultOAuth2AccessToken> tokenResponse1 = action.getTokenResponse(GRANT_TYPE_PASSWORD, ACCOUNT_USERNAME_ROOT, ACCOUNT_PASSWORD_ROOT, client1.getHeaders().getLocation().toString(), oldClient.getClientSecret());
 
         Assert.assertEquals(HttpStatus.OK, tokenResponse1.getStatusCode());
+    }
+
+    @Test
+    public void create_resource_client_and_client_which_access_it_then_delete_resource_client() throws InterruptedException {
+        Client clientAsResource = action.getClientAsResource();
+        ResponseEntity<String> client = action.createClient(clientAsResource);
+        Assert.assertEquals(HttpStatus.OK, client.getStatusCode());
+        String resourceClientId = client.getHeaders().getLocation().toString();
+        Client clientAsNonResource = action.getClientAsNonResource(resourceClientId, CLIENT_ID_OAUTH2_ID);
+        HashSet<ScopeEnum> scopes = new HashSet<>();
+        scopes.add(ScopeEnum.TRUST);
+        clientAsNonResource.setScopeEnums(scopes);
+        HashSet<GrantTypeEnum> enums = new HashSet<>();
+        enums.add(GrantTypeEnum.PASSWORD);
+        enums.add(GrantTypeEnum.REFRESH_TOKEN);
+        clientAsNonResource.setGrantTypeEnums(enums);
+        ResponseEntity<String> client1 = action.createClient(clientAsNonResource);
+        Assert.assertEquals(HttpStatus.OK, client1.getStatusCode());
+        String clientId = client1.getHeaders().getLocation().toString();
+        String clientSecret = clientAsNonResource.getClientSecret();
+        //get jwt
+        ResponseEntity<DefaultOAuth2AccessToken> jwtPasswordWithClient = action.getJwtPasswordWithClient(clientId, clientSecret, ACCOUNT_USERNAME_ADMIN, ACCOUNT_PASSWORD_ADMIN);
+        Assert.assertEquals(HttpStatus.OK, jwtPasswordWithClient.getStatusCode());
+        // clientAsNonResource can access endpoint
+        String url = UserAction.proxyUrl + UserAction.SVC_NAME_AUTH + RESOURCE_OWNER + UserAction.ACCESS_ROLE_ADMIN;
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(jwtPasswordWithClient.getBody().getValue());
+        HttpEntity<String> request = new HttpEntity<>(null, headers);
+        ResponseEntity<SumTotalUser> exchange = action.restTemplate.exchange(url, HttpMethod.GET, request, SumTotalUser.class);
+        Assert.assertEquals(HttpStatus.OK, exchange.getStatusCode());
+        //delete resource client
+        ResponseEntity<DefaultOAuth2AccessToken> tokenResponse = action.getJwtPassword(ACCOUNT_USERNAME_ROOT, ACCOUNT_PASSWORD_ROOT);
+        String bearer = tokenResponse.getBody().getValue();
+        String url4 = UserAction.proxyUrl + UserAction.SVC_NAME_AUTH + CLIENTS + ACCESS_ROLE_ROOT + "/" + resourceClientId;
+        HttpHeaders headers4 = new HttpHeaders();
+        headers4.setBearerAuth(bearer);
+        HttpEntity<String> request4 = new HttpEntity<>(null, headers4);
+        ResponseEntity<String> exchange1 = action.restTemplate.exchange(url4, HttpMethod.DELETE, request4, String.class);
+        Assert.assertEquals(HttpStatus.OK, exchange1.getStatusCode());
+        Thread.sleep(5000);
+        //clientAsNonResource should not have removed client
+        String url5 = UserAction.proxyUrl + UserAction.SVC_NAME_AUTH + CLIENTS + ACCESS_ROLE_ROOT + "/" + clientId;
+        ResponseEntity<Client> exchange3 = action.restTemplate.exchange(url5, HttpMethod.GET, request4, Client.class);
+        Assert.assertEquals(HttpStatus.OK, exchange3.getStatusCode());
+        Set<String> resourceIds = exchange3.getBody().getResourceIds();
+        Assert.assertEquals(1, resourceIds.size());
+        //clientAsNonResource can not access endpoint both access token
+        ResponseEntity<SumTotalUser> exchange2 = action.restTemplate.exchange(url, HttpMethod.GET, request, SumTotalUser.class);
+        Assert.assertEquals(HttpStatus.UNAUTHORIZED, exchange2.getStatusCode());
+        //even refresh token will not work
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "refresh_token");
+        params.add("refresh_token", jwtPasswordWithClient.getBody().getRefreshToken().getValue());
+        HttpHeaders headers2 = new HttpHeaders();
+        headers2.setBasicAuth(clientId, clientSecret);
+        HttpEntity<MultiValueMap<String, String>> request2 = new HttpEntity<>(params, headers2);
+        ResponseEntity<DefaultOAuth2AccessToken> exchange4 = action.restTemplate.exchange(PROXY_URL_TOKEN, HttpMethod.POST, request2, DefaultOAuth2AccessToken.class);
+        Assert.assertEquals(HttpStatus.UNAUTHORIZED, exchange4.getStatusCode());
+        //get new jwt
+        ResponseEntity<DefaultOAuth2AccessToken> jwtPasswordWithClient3 = action.getJwtPasswordWithClient(clientId, clientSecret, ACCOUNT_USERNAME_ADMIN, ACCOUNT_PASSWORD_ADMIN);
+        Assert.assertEquals(HttpStatus.OK, jwtPasswordWithClient3.getStatusCode());
+        // clientAsNonResource can access endpoint again
+        HttpHeaders headers5 = new HttpHeaders();
+        headers5.setBearerAuth(jwtPasswordWithClient3.getBody().getValue());
+        HttpEntity<String> request5 = new HttpEntity<>(null, headers5);
+        ResponseEntity<SumTotalUser> exchange5 = action.restTemplate.exchange(url, HttpMethod.GET, request5, SumTotalUser.class);
+        Assert.assertEquals(HttpStatus.OK, exchange5.getStatusCode());
+    }
+
+    @Test
+    public void create_resource_client_and_client_which_access_it_then_resource_client_is_not_accessible() throws InterruptedException {
+        Client clientAsResource = action.getClientAsResource();
+        ResponseEntity<String> client = action.createClient(clientAsResource);
+        Assert.assertEquals(HttpStatus.OK, client.getStatusCode());
+        String resourceClientId = client.getHeaders().getLocation().toString();
+        Client clientAsNonResource = action.getClientAsNonResource(resourceClientId, CLIENT_ID_OAUTH2_ID);
+        HashSet<ScopeEnum> scopes = new HashSet<>();
+        scopes.add(ScopeEnum.TRUST);
+        clientAsNonResource.setScopeEnums(scopes);
+        HashSet<GrantTypeEnum> enums = new HashSet<>();
+        enums.add(GrantTypeEnum.PASSWORD);
+        enums.add(GrantTypeEnum.REFRESH_TOKEN);
+        clientAsNonResource.setGrantTypeEnums(enums);
+        ResponseEntity<String> client1 = action.createClient(clientAsNonResource);
+        Assert.assertEquals(HttpStatus.OK, client1.getStatusCode());
+        String clientId = client1.getHeaders().getLocation().toString();
+        String clientSecret = clientAsNonResource.getClientSecret();
+        //get jwt
+        ResponseEntity<DefaultOAuth2AccessToken> jwtPasswordWithClient = action.getJwtPasswordWithClient(clientId, clientSecret, ACCOUNT_USERNAME_ADMIN, ACCOUNT_PASSWORD_ADMIN);
+        Assert.assertEquals(HttpStatus.OK, jwtPasswordWithClient.getStatusCode());
+        // clientAsNonResource can access endpoint
+        String url = UserAction.proxyUrl + UserAction.SVC_NAME_AUTH + RESOURCE_OWNER + UserAction.ACCESS_ROLE_ADMIN;
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(jwtPasswordWithClient.getBody().getValue());
+        HttpEntity<String> request = new HttpEntity<>(null, headers);
+        ResponseEntity<SumTotalUser> exchange = action.restTemplate.exchange(url, HttpMethod.GET, request, SumTotalUser.class);
+        Assert.assertEquals(HttpStatus.OK, exchange.getStatusCode());
+        //update resource client to remove access
+        clientAsResource.setResourceIndicator(false);
+        ResponseEntity<DefaultOAuth2AccessToken> tokenResponse = action.getJwtPassword(ACCOUNT_USERNAME_ROOT, ACCOUNT_PASSWORD_ROOT);
+        String bearer = tokenResponse.getBody().getValue();
+        String url4 = UserAction.proxyUrl + UserAction.SVC_NAME_AUTH + CLIENTS + ACCESS_ROLE_ROOT + "/" + resourceClientId;
+        HttpHeaders headers4 = new HttpHeaders();
+        headers4.setBearerAuth(bearer);
+        HttpEntity<Client> request4 = new HttpEntity<>(clientAsResource, headers4);
+        ResponseEntity<String> exchange1 = action.restTemplate.exchange(url4, HttpMethod.PUT, request4, String.class);
+        Assert.assertEquals(HttpStatus.OK, exchange1.getStatusCode());
+        Thread.sleep(5000);
+        //clientAsNonResource can not access endpoint both access token
+        ResponseEntity<SumTotalUser> exchange2 = action.restTemplate.exchange(url, HttpMethod.GET, request, SumTotalUser.class);
+        Assert.assertEquals(HttpStatus.UNAUTHORIZED, exchange2.getStatusCode());
+        //even refresh token will not work
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "refresh_token");
+        params.add("refresh_token", jwtPasswordWithClient.getBody().getRefreshToken().getValue());
+        HttpHeaders headers2 = new HttpHeaders();
+        headers2.setBasicAuth(clientId, clientSecret);
+        HttpEntity<MultiValueMap<String, String>> request2 = new HttpEntity<>(params, headers2);
+        ResponseEntity<DefaultOAuth2AccessToken> exchange4 = action.restTemplate.exchange(PROXY_URL_TOKEN, HttpMethod.POST, request2, DefaultOAuth2AccessToken.class);
+        Assert.assertEquals(HttpStatus.UNAUTHORIZED, exchange4.getStatusCode());
+        //get new jwt
+        ResponseEntity<DefaultOAuth2AccessToken> jwtPasswordWithClient3 = action.getJwtPasswordWithClient(clientId, clientSecret, ACCOUNT_USERNAME_ADMIN, ACCOUNT_PASSWORD_ADMIN);
+        Assert.assertEquals(HttpStatus.OK, jwtPasswordWithClient3.getStatusCode());
+        // clientAsNonResource can access endpoint again
+        HttpHeaders headers5 = new HttpHeaders();
+        headers5.setBearerAuth(jwtPasswordWithClient3.getBody().getValue());
+        HttpEntity<String> request5 = new HttpEntity<>(null, headers5);
+        ResponseEntity<SumTotalUser> exchange5 = action.restTemplate.exchange(url, HttpMethod.GET, request5, SumTotalUser.class);
+        Assert.assertEquals(HttpStatus.OK, exchange5.getStatusCode());
     }
 
 }
---
+++ src/main/java/com/hw/integration/identityaccess/oauth2/RefreshTokenTest.java
@@ -0,0 +1,138 @@
+package com.hw.integration.identityaccess.oauth2;
+
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hw.helper.*;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.*;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.UUID;
+
+import static com.hw.helper.UserAction.*;
+import static com.hw.integration.identityaccess.oauth2.BIzUserTest.RESOURCE_OWNER;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class RefreshTokenTest {
+    public ObjectMapper mapper = new ObjectMapper().configure(MapperFeature.USE_ANNOTATIONS, false);
+    @Autowired
+    private UserAction action;
+    UUID uuid;
+    @Rule
+    public TestWatcher watchman = new TestWatcher() {
+        @Override
+        protected void failed(Throwable e, Description description) {
+            action.saveResult(description, uuid);
+            log.error("test failed, method {}, uuid {}", description.getMethodName(), uuid);
+        }
+    };
+
+    @Before
+    public void setUp() {
+        uuid = UUID.randomUUID();
+        action.restTemplate.getRestTemplate().setInterceptors(Collections.singletonList(new OutgoingReqInterceptor(uuid)));
+    }
+
+    @Test
+    public void refresh_token_should_work() throws InterruptedException {
+        //create client supports refresh token
+        Client clientRaw = action.getClientRaw();
+        String clientSecret = clientRaw.getClientSecret();
+        HashSet<GrantTypeEnum> enums = new HashSet<>();
+        enums.add(GrantTypeEnum.PASSWORD);
+        enums.add(GrantTypeEnum.REFRESH_TOKEN);
+        clientRaw.setResourceIds(Collections.singleton(CLIENT_ID_OAUTH2_ID));
+        clientRaw.setGrantTypeEnums(enums);
+        clientRaw.setGrantedAuthorities(List.of(ClientAuthorityEnum.ROLE_FRONTEND));
+        HashSet<ScopeEnum> scopes = new HashSet<>();
+        scopes.add(ScopeEnum.TRUST);
+        clientRaw.setScopeEnums(scopes);
+        clientRaw.setAccessTokenValiditySeconds(10);
+        clientRaw.setRefreshTokenValiditySeconds(100);
+        ResponseEntity<String> client = action.createClient(clientRaw);
+        String clientId = client.getHeaders().getLocation().toString();
+        Assert.assertEquals(HttpStatus.OK, client.getStatusCode());
+        //get jwt
+        ResponseEntity<DefaultOAuth2AccessToken> jwtPasswordWithClient = action.getJwtPasswordWithClient(clientId, clientSecret, ACCOUNT_USERNAME_ADMIN, ACCOUNT_PASSWORD_ADMIN);
+        Assert.assertEquals(HttpStatus.OK, jwtPasswordWithClient.getStatusCode());
+        //access endpoint
+        String url = UserAction.proxyUrl + UserAction.SVC_NAME_AUTH + RESOURCE_OWNER + UserAction.ACCESS_ROLE_ADMIN;
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(jwtPasswordWithClient.getBody().getValue());
+        HttpEntity<String> request = new HttpEntity<>(null, headers);
+        ResponseEntity<SumTotalUser> exchange = action.restTemplate.exchange(url, HttpMethod.GET, request, SumTotalUser.class);
+        Assert.assertEquals(HttpStatus.OK, exchange.getStatusCode());
+        Thread.sleep(11000);
+        //access access token should expire
+        ResponseEntity<SumTotalUser> exchange2 = action.restTemplate.exchange(url, HttpMethod.GET, request, SumTotalUser.class);
+        Assert.assertEquals(HttpStatus.UNAUTHORIZED, exchange2.getStatusCode());
+        //get access token with refresh token
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "refresh_token");
+        params.add("refresh_token", jwtPasswordWithClient.getBody().getRefreshToken().getValue());
+        HttpHeaders headers2 = new HttpHeaders();
+        headers2.setBasicAuth(clientId, clientSecret);
+        HttpEntity<MultiValueMap<String, String>> request2 = new HttpEntity<>(params, headers2);
+        ResponseEntity<DefaultOAuth2AccessToken> exchange1 = action.restTemplate.exchange(PROXY_URL_TOKEN, HttpMethod.POST, request2, DefaultOAuth2AccessToken.class);
+        Assert.assertEquals(HttpStatus.OK, exchange1.getStatusCode());
+        //use new access token for api call
+        HttpHeaders headers3 = new HttpHeaders();
+        headers3.setBearerAuth(exchange1.getBody().getValue());
+        HttpEntity<String> request3 = new HttpEntity<>(null, headers3);
+        ResponseEntity<SumTotalUser> exchange3 = action.restTemplate.exchange(url, HttpMethod.GET, request3, SumTotalUser.class);
+        Assert.assertEquals(HttpStatus.OK, exchange3.getStatusCode());
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
